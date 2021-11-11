e78b59fcc5309ed1d47fe9d15869ac2bd81625cc
John_Ballen <jhonn.ballen@gmail.com>
2021-11-11 15:18:59 -0500

    python basics - POO

---
+++ Curso/Leccion1/main.py
@@ -0,0 +1,7 @@
+condicion = int(input("Cuanto es 8 x 8:"))
+if condicion == 64:
+    print ("Es correcto")
+elif condicion != 64:
+    print ("No es correcto")
+else:
+    print ("Condicion no reconocida")
