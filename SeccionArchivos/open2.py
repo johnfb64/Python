eea8ccd82044b285b0067162753006347c23d652
johnfb64 <jhonn.ballen@gmail.com>
2022-05-12 18:45:43 -0500

    Delete SeccionArchivos directory

--- SeccionArchivos/open2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#_*_ coding: utf8 _*_
-
-#r  = modo lectura
-
-#metodo lee y hace split de datos en archivo. Split eliminara el \n. 
-archivo = open('archivo1.txt','r')
-for l in archivo.read().split():
-    print(l)
